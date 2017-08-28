8f1dfb6fa4cd89120ac9913708ee8c06b669cbd7
Ikeda Fumie <ikefumi0707@gmail.com>
2017-08-28 10:42:12 +0900

    heroku7

--- pj1/settings.py
+++ pj1/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'pj1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+DATABASES['default']  = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
